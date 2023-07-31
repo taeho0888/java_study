be154b5b673c9439e33ba941a30978f674b6bd79
taeho0888 <taeho0888@naver.com>
2023-07-31 21:37:57 +0900

    코드 추가

--- CarProject/src/Road.java
+++ CarProject/src/Road.java
@@ -4,7 +4,11 @@ public class Road {
     private ArrayList<CarInterface> carsOnRoad;
 
     public Road(){
+        this.carsOnRoad = new ArrayList<>();
+    }
 
+    public Road(ArrayList<CarInterface> carsOnRoad) {
+        this.carsOnRoad = carsOnRoad;
     }
 
     public void addCar(CarInterface car) {
