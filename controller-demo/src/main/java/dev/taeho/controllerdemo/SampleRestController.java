8d4e90f623c284c12f10a043308da5295519bcc4
taeho0888 <taeho0888@naver.com>
2023-08-07 00:51:53 +0900

    techit java 3-4

--- controller-demo/src/main/java/dev/taeho/controllerdemo/SampleRestController.java
+++ controller-demo/src/main/java/dev/taeho/controllerdemo/SampleRestController.java
@@ -2,10 +2,10 @@ package dev.taeho.controllerdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
 import java.io.FileInputStream;
@@ -18,9 +18,32 @@ public class SampleRestController {
      private static final Logger logger = LoggerFactory.getLogger(SampleRestController.class );
 
      @GetMapping("/sample-payload")
-    public SamplePayload samplePayload() {
-          return new SamplePayload("taeho", 25, "student");
+    public SamplePayload samplePayloadGet() {
+         return new SamplePayload("taeho", 25, "student");
      }
+
+    @PostMapping("/sample-payload")
+    @ResponseStatus(HttpStatus.NO_CONTENT ) // 정상적으로 로직이 처리되었을 떄의 결괏값 세팅
+    public void samplePayloadPost(@RequestBody SamplePayload samplePayload) {
+         logger.info(samplePayload.toString() );
+    }
+
+    @PostMapping(
+            value = "/sample-multipart",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void sampleMultipartPost(
+            @RequestParam("name") String name,
+            @RequestParam("age") Integer age,
+            @RequestParam("name") String occupation,
+            @RequestParam("file")MultipartFile multipartFile
+            ) {
+        logger.info("name: " + name);
+        logger.info("age: " + age);
+        logger.info("occupation: " + occupation);
+        logger.info("file: " + multipartFile.getOriginalFilename());
+    }
 
      @GetMapping(
              value = "/sample-image",
@@ -32,4 +55,6 @@ public class SampleRestController {
          assert inputStream != null;
          return inputStream.readAllBytes();
      }
+
+
 }
